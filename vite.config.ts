cba5dc3f245620f82197659ed4e0061cb0795328
aifianS77 <163632977+aifianS77@users.noreply.github.com>
2026-02-21 11:00:59 +0800

    update vite.config.ts for github

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Henna/',   // 👈 ADD THIS LINE
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 3000,
     open: true,
   },
+
   build: {
     outDir: 'dist',
     sourcemap: true,
